87fcf98010d9ad27b01073d10052f86024956f43
krishnasubedi <47682398+Krishnasubedi1998@users.noreply.github.com>
2019-05-17 17:42:05 -0400

    Update main.tf

--- east/main.tf
+++ east/main.tf
@@ -2,12 +2,23 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "web1" {
-  ami = "${var.ami_image}"
-  instance_type = "${var.instance_type}"
-  key_name = "${var.key_name}"
-  security_groups = ["${var.security_groups}"]
-  tags {
-    Name = "Instance1"
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "default"
+
+  ingress {
+    # TLS (change to whatever ports you need)
+    from_port   = 443
+    to_port     = 443
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = # add a CIDR block here
   }
- }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
